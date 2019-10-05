50dde091f6da89fab13a4c6a155743ae767f1905
vavo 22 <varand22@gmail.com>
2019-10-05 22:44:56 +0330

    Add Items

--- app/src/main/java/com/varand/sematecvarandproject/MainActivity.java
+++ app/src/main/java/com/varand/sematecvarandproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.varand.sematecvarandproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("VarandTags","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("VarandTags","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("VarandTags","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("VarandTags","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("VarandTags","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("VarandTags","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("VarandTags","onRestart");
     }
 }
